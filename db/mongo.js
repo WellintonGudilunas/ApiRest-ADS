59b8c69f4a89d20fdd2cdcb1b67ed9f5d594ef36
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-04-11 23:58:43 -0300

    Projeto inicial sem validações e sem front end

--- db/mongo.js
+++ db/mongo.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const url = 'mongodb://127.0.0.1:27017/meubanco';
 
-//const url = 'mongodb+srv://admin:senha@meubanco.opfdf.mongodb.net/?retryWrites=true&w=majority';
 
 const db = mongoose.connect(url);
 
