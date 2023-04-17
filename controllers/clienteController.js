acc801bdd6703ffe3d704d5d307d98be77187d1d
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-04-16 22:33:52 -0300

    Validações

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -5,6 +5,12 @@ class clienteController{
     async listar(req, res){  
         //select * from cliente;  
         const resultado = await clienteModel.find({});
+
+        if(!resultado || resultado.length === 0){
+            res.status(404).json({msg: "Não há nenhum cliente cadastrado!."});
+            return;
+        }
+
         res.json(resultado);    
     }
 
@@ -12,12 +18,23 @@ class clienteController{
         const codigo  = req.params.codigo;
         //select * from cliente where codigo = 2;
         const resultado = await clienteModel.findOne({'codigo': codigo});
+        if(!resultado){
+            res.status(404).json({msg: "Cliente não encontrado."});
+            return;
+        }
+
         res.json(resultado);
     }
 
-    async salvar(req, res){
-        const cliente = req.body;
 
+    // FAZER VALIDAÇÃO SE ESTÁ FALTANDO ALGUM CAMPO
+    async salvar(req, res){
+        const cliente = {
+            nome: req.body.nome,
+            idade: req.body.idade,
+            cep: req.body.cep
+        }
+        
         //Gerador de novo código
         //select * from cliente order by codigo desc;
         const objeto = await clienteModel.findOne({}).sort({'codigo': -1});
@@ -28,12 +45,34 @@ class clienteController{
         res.json(resultado);        
     }
 
+
+    // FAZER VALIDACAO SE ATUALIZOU ALGUMA COISA
+    // FAZER VALIDACAO SE O CONTEUDO A SER ATUALIZADO EXISTE
     async atualizar(req, res){
         const codigo = req.params.codigo;
-        const cliente = req.body;
+        const cliente = {
+            nome: req.body.nome,
+            idade: req.body.idade,
+            cep: req.body.cep
+        }
+        
         //update cliente set xxxx values xxxx
-        await clienteModel.findOneAndUpdate({'codigo': codigo}, cliente);
-        res.send("Conteúdo atualizado!");
+        const updatedCliente = await clienteModel.findOneAndUpdate({'codigo': codigo}, cliente);
+        if(!updatedCliente){
+            res.status(404).json({msg: "Cliente não encontrado!."});
+            return;
+        }
+
+        //VALIDAR SE ALGUM DADO FOI ATUALIZADO OU SE FALTA ALGUM CAMPO A ATUALIZAR
+        // console.log(updatedCliente);
+        // if (updatedCliente === null) {
+        //     console.log('Nenhum documento foi atualizado.');
+        //     res.status(200).json({cliente, msg: "Nenhum documento foi atualizado. (Os dados informados são exatamente iguais aos do cliente!)." });
+        // } else {
+        //     console.log('Documento(s) atualizado(s):', updatedCliente.nModified);
+        //     res.status(200).json({cliente, msg: "Cliente Atualizado com sucesso!" });
+        // }
+
     }
 
     async excluir(req, res){
