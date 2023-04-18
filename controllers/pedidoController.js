5205c162e05a84a25e5b099acd432744f4fbf762
Tiago André Lachman <tiagolachman@gmail.com>
2023-04-18 15:35:12 -0300

    try catch em todos

--- controllers/pedidoController.js
+++ controllers/pedidoController.js
@@ -11,76 +11,82 @@ class pedidoController {
     }
 
     async listar(req, res) {
-        //select * from pedido;  
-        const resultado = await pedidoModel.find({});
+        try {
+            //select * from pedido;  
+            const resultado = await pedidoModel.find({});
 
-        if (!resultado || resultado.length === 0) {
-            res.status(400).json({ msg: "Não há nenhum pedido cadastrado!." });
-            return;
-        }
-
-        res.json(resultado);
-    }
-
-    async buscarPorCodigo(req, res) {
-        const codigo = req.params.codigo;
-        //select * from pedido where codigo = 2;
-        const resultado = await pedidoModel.findOne({ 'codigo': codigo });
-        if (!resultado) {
-            res.status(400).json({ msg: `Pedido com código ${codigo} não encontrado.` });
-            return;
-        }
-
-        res.json(resultado);
-        return resultado;
-    }
-
-    async cadastrar(req, res) {
-        const pedido = req.body;
-
-        if (pedido.produtos.length !== pedido.quantidade.length) {
-            res.status(400).json({ msg: "Erro no tamanho dos vetores" });
-            return;
-        }
-
-        //REFERENCIA 
-        const cliente = await clienteModel.findOne({ 'codigo': pedido.codigoCliente })
-        if (cliente === null) {
-            res.status(400).json({ msg: `Cliente com código ${pedido.codigoCliente} não encontrado.` });
-            return;
-        }
-        pedido.codigoCliente = cliente._id;
-        pedido.codigoProduto = [];
-
-        for (let i = 0; i < pedido.produtos.length; i++) {
-            const cod = pedido.produtos[i];
-            let p = await produtoModel.findOne({ 'codigo': cod });
-
-            if (!p) {
-                res.status(400).json({ msg: `O produto com código ${cod} é inexistente` });
+            if (!resultado || resultado.length === 0) {
+                res.status(400).json({ msg: "Não há nenhum pedido cadastrado!." });
                 return;
             }
 
-            pedido.codigoProduto[i] = p._id;
+            res.json(resultado);
+        } catch (err) {
+            res.status(500).json({ msg: "Erro interno" });
         }
-        //Removendo o json produtos
-        pedido.produtos = undefined;
+    }
 
-        //Gerador de novo código
-        //select * from pedido order by codigo desc;
-        const objeto = await pedidoModel.findOne({}).sort({ 'codigo': -1 });
-        pedido.codigo = objeto == null ? 1 : objeto.codigo + 1;
-
-        //Esse aq é um testezinho pq no postman fica mais facil de ver
-        //res.json(pedido);
-        //return;
-
+    async buscarPorCodigo(req, res) {
         try {
+            const codigo = req.params.codigo;
+            //select * from pedido where codigo = 2;
+            const resultado = await pedidoModel.findOne({ 'codigo': codigo });
+            if (!resultado) {
+                res.status(400).json({ msg: `Pedido com código ${codigo} não encontrado.` });
+                return;
+            }
+
+            res.json(resultado);
+            return resultado;
+        } catch (err) {
+            res.status(500).json({ msg: "Erro interno" });
+        }
+    }
+
+    async cadastrar(req, res) {
+        try {
+            const pedido = req.body;
+
+            if (pedido.produtos.length !== pedido.quantidade.length) {
+                res.status(400).json({ msg: "Erro no tamanho dos vetores" });
+                return;
+            }
+
+            //REFERENCIA 
+            const cliente = await clienteModel.findOne({ 'codigo': pedido.codigoCliente })
+            if (cliente === null) {
+                res.status(400).json({ msg: `Cliente com código ${pedido.codigoCliente} não encontrado.` });
+                return;
+            }
+            pedido.codigoCliente = cliente._id;
+            pedido.codigoProduto = [];
+
+            for (let i = 0; i < pedido.produtos.length; i++) {
+                const cod = pedido.produtos[i];
+                let p = await produtoModel.findOne({ 'codigo': cod });
+
+                if (!p) {
+                    res.status(400).json({ msg: `O produto com código ${cod} é inexistente` });
+                    return;
+                }
+
+                pedido.codigoProduto[i] = p._id;
+            }
+            //Removendo o json produtos
+            pedido.produtos = undefined;
+
+            //Gerador de novo código
+            //select * from pedido order by codigo desc;
+            const objeto = await pedidoModel.findOne({}).sort({ 'codigo': -1 });
+            pedido.codigo = objeto == null ? 1 : objeto.codigo + 1;
+
+            //Esse aq é um testezinho pq no postman fica mais facil de ver
+            //res.json(pedido);
+            //return;
             const resultado = await pedidoModel.create(pedido);
             res.json(resultado);
         } catch (err) {
-            console.log(err);
-            res.json(err);
+            res.status(500).json({ msg: "Erro interno" });
         }
     }
 
@@ -130,31 +136,28 @@ class pedidoController {
             //res.json(retorno);
             res.send("Conteúdo atualizado!");
         } catch (err) {
-            res.status(500).json({msg:"Erro interno"});
+            res.status(500).json({ msg: "Erro interno" });
         }
     }
 
-
-    //REVISAR ISSO
-    /*
-    *
-    */
     async excluir(req, res) {
+        try {
+            const codigo = req.params.codigo;
 
-        const dados = await pedidoModel.findOne({ 'codigo': req.params.codigo });
-
-        if (dados === null) {
-            res.status(400).send("Código não encontrado");
-            return;
+            let retorno = await pedidoModel.findOneAndDelete({ 'codigo': codigo });
+            if (retorno === null) {
+                res.status(400).json({ msg: `O pedido com código ${codigo} é inexistente` });
+                return;
+            }
+            retorno = await pedidoModel.findOne({ 'codigo': codigo });
+            if (retorno != null) {
+                res.status(400).json({ msg: `O pedido com código ${codigo} não foi excluid com exito` });
+                return;
+            }
+            res.send("Conteúdo excluído!");
+        } catch (err) {
+            res.status(500).json({ msg: "Erro interno" });
         }
-        const codigo = req.params.codigo;
-
-        const retorno = await pedidoModel.findOneAndDelete({ 'codigo': codigo });
-        if (retorno == null) {
-            res.send("Conteúdo não encontrado");
-            return;
-        }
-        res.send("Conteúdo excluído!");
     }
 
 }
