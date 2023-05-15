a7fbd5d7f3bcc267a842ffd083ec386bbcdbcf84
WellintonGudilunas <wellintongudilunas788@gmail.com>
2023-05-14 21:12:12 -0300

    Commit inicio Frontend, estrutura basica react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <>
+      <header>
+		  <h1>Matrix</h1>
+      </header>
+      <nav>
+        <ul>
+          <li><a href="">Nav 1</a></li>
+          <li><a href="">Nav 2</a></li>
+          <li><a href="">Nav 3</a></li>
+        </ul>
+      </nav>
+      <main>
+        <h2>Área do conteúdo principal</h2>
+        <p>Neste layout, são exibidas as áreas na ordem de origem para
+          qualquer tela com menos de 500 pixels de largura. Vamos para uma coluna de duas
+          layout e, em seguida, para um layout de três colunas redefinindo o
+          grade e a colocação de itens na grade.</p>
+      </main>
+      <aside>Barra lateral</aside>
+      <footer>Rodapé</footer>
+    </>
+  );
+}
+
+export default App;
